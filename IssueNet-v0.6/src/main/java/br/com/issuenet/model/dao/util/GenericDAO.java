0b62424102bae015653d07d53cc3c97c6b553aa1
bensilva <benalves@gmail.com>
2015-01-17 20:52:44 -0200

    GenericDAO update

--- IssueNet-v0.6/src/main/java/br/com/issuenet/model/dao/util/GenericDAO.java
+++ IssueNet-v0.6/src/main/java/br/com/issuenet/model/dao/util/GenericDAO.java
@@ -2,17 +2,38 @@ package br.com.issuenet.model.dao.util;
 
 import java.io.Serializable;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class GenericDAO {
 	public static void genericSave(Serializable s) {
+		Session session = createSession();		
+		try{
+		session.save(s);
+		commitSession(session);
+		}catch(HibernateException e){
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+	}
+	
+	public static void genericUpdate(Serializable s) {
+		Session session = createSession();		
+		session.update(s);
+		commitSession(session);
+	}
+
+	private static Session createSession() {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		
-		session.save(s);
+		return session;
+	}
+	
+	private static void commitSession(Session session) {
 		session.getTransaction().commit();
 		session.close();
 	}
+	
 }
